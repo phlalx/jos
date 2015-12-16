072d14138f7ff9c0d29e06efe44fd18ece1e00bb
phlalx <phlalx@users.noreply.github.com>
2015-12-16 20:44:04 +0100

    exercise 5 ok

--- kern/e1000.c
+++ kern/e1000.c
@@ -3,19 +3,31 @@
 
 volatile uint32_t *e1000;
 
-// static void
-// e1000w(int index, int value)
-// {
-// 	e1000[index] = value;
-// }
+#define TDLEN 16 // TDLEN * sizeof(tx_desc) must be a multiple of 128
+
+uint8_t send_buffer[TDLEN][MTU];
+// align this on 16-bytes boundary
+
+struct tx_desc descs[TDLEN];
 
 int e1000_attach_fn(struct pci_func *pcif) {
 	pci_func_enable(pcif);
 	uint32_t base = pcif->reg_base[0];
 	uint32_t size = pcif->reg_size[0];
 	e1000 = mmio_map_region(base, size);
-	uint32_t val = e1000[E1000_STATUS >> 2];
+	uint32_t val = e1000[E1000_STATUS];
 	cprintf("E1000 Status = %08x\n", val);
 
+	e1000[E1000_TDBAL] = (uint32_t) PADDR(descs); 
+	e1000[E1000_TDLEN] = TDLEN * sizeof(struct tx_desc);
+	e1000[E1000_TDH] = 0;
+	e1000[E1000_TDT] = 0;
+	e1000[E1000_TCTL] = E1000_TCTL_EN | E1000_TCTL_PSP | 0x10 << E1000_TCTL_CT_SHIFT
+	| 0x40 << E1000_TCTL_COLD_SHIFT; // cf. section 14.5 et exercise 5
+
+	e1000[E1000_TIPG] = 10 << E1000_IGPT_SHIFT | 10 << E1000_IGPR1_SHIFT 
+	| 10 << E1000_IGPR2_SHIFT; // TODO revoir totalement ça ! 10.4.34
+
+	// TODO que renvoyer ?
 	return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -18,8 +18,51 @@
  * R/clr - register is read only and is cleared when read
  * A - register array
  */
-#define E1000_CTRL     0x00000  /* Device Control - RW */
-#define E1000_STATUS   0x00008  /* Device Status - RO */
+#define E1000_CTRL     0x00000      /* Device Control - RW */
+#define E1000_STATUS   0x00008 / 4  /* Device Status - RO */
+#define E1000_TDBAL    0x03800 / 4 
+#define E1000_TDLEN    0x03808 / 4 /* TX Descriptor Length - RW */
+#define E1000_TDH      0x03810 / 4 /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818 / 4 /* TX Descripotr Tail - RW */
+#define E1000_TCTL     0x00400 / 4 /* TX Control - RW */
+#define E1000_TIPG     0x00410 / 4 /* TX Inter-packet gap -RW */
+
+/* Transmit Control */
+#define E1000_TCTL_RST    0x00000001    /* software reset */
+#define E1000_TCTL_EN     0x00000002    /* enable tx */
+#define E1000_TCTL_BCE    0x00000004    /* busy check enable */
+#define E1000_TCTL_PSP    0x00000008    /* pad short packets */
+#define E1000_TCTL_CT     0x00000ff0    /* collision threshold */
+#define E1000_TCTL_CT_SHIFT 4
+
+#define E1000_IGPT_SHIFT 0
+#define E1000_IGPR1_SHIFT 10 
+#define E1000_IGPR2_SHIFT 20 
+
+#define E1000_TCTL_COLD   0x003ff000    /* collision distance */
+#define E1000_TCTL_COLD_SHIFT 12
+#define E1000_TCTL_SWXOFF 0x00400000    /* SW Xoff transmission */
+#define E1000_TCTL_PBE    0x00800000    /* Packet Burst Enable */
+#define E1000_TCTL_RTLC   0x01000000    /* Re-transmit on late collision */
+#define E1000_TCTL_NRTU   0x02000000    /* No Re-transmit on underrun */
+#define E1000_TCTL_MULR   0x10000000    /* Multiple request support */
+ 
+#define E1000_IGPT_SHIFT 0
+#define E1000_IGPR1_SHIFT 10 
+#define E1000_IGPR2_SHIFT 20 
+ 
+#define MTU 1518
+
+struct tx_desc
+{
+	uint64_t addr;
+	uint16_t length;
+	uint8_t cso;
+	uint8_t cmd;
+	uint8_t status;
+	uint8_t css;
+	uint16_t special;
+};
 
 int e1000_attach_fn(struct pci_func *pcif);
 
