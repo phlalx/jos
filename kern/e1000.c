c1c5be0ed17ec5b5113bc64ed377b992a7983576
phlalx <phlalx@users.noreply.github.com>
2015-12-16 13:51:52 +0100

    exercise 3 - e1000 enabled

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,3 +1,6 @@
 #include <kern/e1000.h>
 
-// LAB 6: Your driver code here
+int e1000_attach_fn(struct pci_func *pcif) {
+	pci_func_enable(pcif);
+	return 0;
+}
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,4 +1,8 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
 
+#include "pci.h"
+
+int e1000_attach_fn(struct pci_func *pcif);
+
 #endif	// JOS_KERN_E1000_H
