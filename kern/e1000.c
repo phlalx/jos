93f1cebf290310c5b25137ea57365b20fca651cc
phlalx <phlalx@users.noreply.github.com>
2015-12-16 14:27:09 +0100

    exercise 4 ok - IO memory mapping of e1000

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,6 +1,21 @@
 #include <kern/e1000.h>
+#include <kern/pmap.h>
+
+volatile uint32_t *e1000;
+
+// static void
+// e1000w(int index, int value)
+// {
+// 	e1000[index] = value;
+// }
 
 int e1000_attach_fn(struct pci_func *pcif) {
 	pci_func_enable(pcif);
+	uint32_t base = pcif->reg_base[0];
+	uint32_t size = pcif->reg_size[0];
+	e1000 = mmio_map_region(base, size);
+	uint32_t val = e1000[E1000_STATUS >> 2];
+	cprintf("E1000 Status = %08x\n", val);
+
 	return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -3,6 +3,24 @@
 
 #include "pci.h"
 
+#define E1000_DEV_ID_82540EM 0x100E
+#define E1000_VENDOR_ID	0x8086
+
+/* Register Set. (82543, 82544)
+ *
+ * Registers are defined to be 32 bits and  should be accessed as 32 bit values.
+ * These registers are physically located on the NIC, but are mapped into the
+ * host memory address space.
+ *
+ * RW - register is both readable and writable
+ * RO - register is read only
+ * WO - register is write only
+ * R/clr - register is read only and is cleared when read
+ * A - register array
+ */
+#define E1000_CTRL     0x00000  /* Device Control - RW */
+#define E1000_STATUS   0x00008  /* Device Status - RO */
+
 int e1000_attach_fn(struct pci_func *pcif);
 
 #endif	// JOS_KERN_E1000_H
